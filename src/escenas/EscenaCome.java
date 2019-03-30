6293ec382cada833649ec647a03eec36cec2fda6
Itejada <ismael.elpuig@gmail.com>
2019-03-30 01:20:55 +0100

    Packman

--- src/escenas/EscenaCome.java
+++ src/escenas/EscenaCome.java
@@ -4,9 +4,12 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import sprites.Sprite2;
 
@@ -16,21 +19,29 @@ public class EscenaCome extends Application {
 
     private double x = 0;
     private double y = 0;
-    double velocidad = 0.5;
-
+    double velocidad = 7;
+    private final int ALTURA = 650;
+    private final int ANCHO = 450;
 
 
     @Override
     public void start(Stage theStage) {
-        Sprite2 sun= new Sprite2();
-        sun.setImage(new Image("img/sun.png"));
+        Sprite2 sun = new Sprite2();
+        sun.setImage(new Image("img/pacbol_0.png"));
         theStage.setTitle("Keyboard Example");
+        ImageView iv = new ImageView("img/pacbol_0.png");
+
+        Image left = new Image("img/pacbol_2.png");
+        Image rigth = new Image("img/pacbol_1.png");
+        Image up = new Image("img/pacbol_3.png");
+        Image down = new Image("img/pacbol_4.png");
+
 
         Group root = new Group();
         Scene theScene = new Scene(root);
         theStage.setScene(theScene);
 
-        Canvas canvas = new Canvas(1112 - 64, 956);
+        Canvas canvas = new Canvas(ALTURA, ANCHO);
         root.getChildren().add(canvas);
 
         ArrayList<String> input = new ArrayList<String>();
@@ -59,29 +70,49 @@ public class EscenaCome extends Application {
         new AnimationTimer() {
             public void handle(long currentNanoTime) {
                 // Clear the canvas
-                gc.clearRect(0, y-10, 1112, 512);
+                gc.clearRect(0, y - 10, 1112, 512);
 
-                if (input.contains("LEFT") != input.contains("RIGHT")) {
-                    if (input.contains("LEFT")) {
-                        x -= velocidad;
+                    if (input.contains("LEFT") != input.contains("RIGHT")) {
+                        if (input.contains("LEFT")) {
+                            x -= velocidad;
+                            sun.setImage(left);
+
+                            //iv.setRotate(-90);
+
+                        }
+                        if (input.contains("RIGHT")) {
+                            x += velocidad;
+                            sun.setImage(rigth);
+
+                            // iv.setRotate(90);
+
+                        }
                     }
-                    if (input.contains("RIGHT")) {
-                        x += velocidad;
+                    if (input.contains("UP") != input.contains("DOWN")) {
+                        if (input.contains("UP")) {
+                            y -= velocidad;
+                            sun.setImage(up);
+                            // iv.setRotate(0);
+
+                        }
+                        if (input.contains("DOWN")) {
+                            y += velocidad;
+                            sun.setImage(down);
+                            //iv.setRotate(180);
+
+                        }
                     }
-                }
-                if (input.contains("UP") != input.contains("DOWN")) {
-                    if (input.contains("UP")) {
-                        y -= velocidad;
-                    }
-                    if (input.contains("DOWN")) {
-                        y += velocidad;
-                    }
-                }
-                    sun.render(gc);
+
+                sun.render(gc);
+
+                SnapshotParameters params = new SnapshotParameters();
+                params.setFill(Color.TRANSPARENT);
+                Image rotatedImage = iv.snapshot(params, null);
+                //gc.drawImage(rotatedImage,x,y, 50,50);
                 sun.update(0);
                 sun.setPositionX(x);
                 sun.setPositionY(y);
-                   // gc.drawImage(leftG, x, y);
+                // gc.drawImage(leftG, x, y);
 
             }
         }.start();
